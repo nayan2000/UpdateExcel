8912335092b11e604ec95640e36d1b49fefbd2a4
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-07-02 15:20:19 +0530

    Add files via upload

---
+++ UpdateExcel.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import openpyxl
+
+def getStringInput(input_message):
+    while True:
+        string = input(input_message)
+        if string:
+            break
+    return string
+
+
+def getNumericInput(input_message):
+    while(True):
+        number = input(input_message)
+        try:
+            number = int(number)
+            break
+        except Exception as ValueError:
+            pass
+    
+    return number
+
+def get_input():
+    product_name = getStringInput('Please enter product name: ')
+    hsn_code = getNumericInput('Please enter HSN Code of product: ')
+    rate = getNumericInput('Please enter rate of product: ')
+    gst_rate = getNumericInput('Please enter the GST Rate(without % sign): ')
+    
+    return product_name, hsn_code, rate, gst_rate
+
+def update_excel(product_name, hsn_code, rate, gst_rate):
+    file_path = 'sample.xlsx'
+    #this file_path can be full path also, like
+    #file_path = 'F:\My Files\Excels\sample.xlsx'
+    book = openpyxl.load_workbook(file_path)
+    sheet = book.active
+    sheet.append((product_name, hsn_code, rate, gst_rate))
+    book.save(file_path)
+    
+
+#Take input  
+product_name, hsn_code, rate, gst_rate = get_input()
+#Update these values to excel
+update_excel(product_name, hsn_code, rate, gst_rate)
